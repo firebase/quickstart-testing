2699726ecdf083403fd492f53d22a67e7a065310
Sam <samstern@google.com>
2020-08-26 13:42:47 +0100

    Move firebase/testing to firebase/rules-unit-testing

--- unit-test-security-rules/test/database.spec.js
+++ unit-test-security-rules/test/database.spec.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const firebase = require("@firebase/testing");
+const firebase = require("@firebase/rules-unit-testing");
 const fs = require("fs");
 
 /**
