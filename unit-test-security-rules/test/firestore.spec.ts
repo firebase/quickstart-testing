ffb2023f9ca974eb961352a84411cfac35efc7e7
Thomas Bouldin <inlined@google.com>
2026-07-01 12:21:18 -0700

    Update dependencies, engine requirements, fix Firestore emulator fallback, and add local package-lock.json for unit-test-security-rules

--- unit-test-security-rules/test/firestore.spec.ts
+++ unit-test-security-rules/test/firestore.spec.ts
@@ -13,19 +13,35 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import { describe, test, beforeEach, beforeAll, afterAll, expect } from '@jest/globals';
-import { initializeTestEnvironment, RulesTestEnvironment, assertSucceeds } from '@firebase/rules-unit-testing';
-import { serverTimestamp } from 'firebase/firestore'
-import { expectFirestorePermissionDenied, expectFirestorePermissionUpdateSucceeds, getFirestoreCoverageMeta, expectPermissionGetSucceeds } from './utils';
+import {
+  describe,
+  test,
+  beforeEach,
+  beforeAll,
+  afterAll,
+  expect,
+} from "@jest/globals";
+import {
+  initializeTestEnvironment,
+  RulesTestEnvironment,
+  assertSucceeds,
+} from "@firebase/rules-unit-testing";
+import { serverTimestamp } from "firebase/firestore";
+import {
+  expectFirestorePermissionDenied,
+  expectFirestorePermissionUpdateSucceeds,
+  getFirestoreCoverageMeta,
+  expectPermissionGetSucceeds,
+} from "./utils";
 const { readFileSync, createWriteStream } = require("node:fs");
 const { get } = require("node:http");
-import { resolve } from 'node:path';
+import { resolve } from "node:path";
 
 /**
  * The emulator will accept any project ID for testing.
  */
 const PROJECT_ID = "fakeproject";
-const FIREBASE_JSON = resolve(__dirname, '../firebase.json');
+const FIREBASE_JSON = resolve(__dirname, "../firebase.json");
 let testEnv: RulesTestEnvironment;
 
 beforeAll(async () => {
@@ -35,7 +51,7 @@ beforeAll(async () => {
     firestore: {
       port,
       host,
-      rules: readFileSync('firestore.rules', 'utf8')
+      rules: readFileSync("firestore.rules", "utf8"),
     },
   });
 });
@@ -48,7 +64,7 @@ beforeEach(async () => {
 afterAll(async () => {
   // Write the coverage report to a file
   const { coverageUrl } = getFirestoreCoverageMeta(PROJECT_ID, FIREBASE_JSON);
-  const coverageFile = './firestore-coverage.html';
+  const coverageFile = "./firestore-coverage.html";
   const fstream = createWriteStream(coverageFile);
   await new Promise((resolve, reject) => {
     get(coverageUrl, (res) => {
@@ -62,83 +78,87 @@ afterAll(async () => {
 
 describe("My app", () => {
   test("require users to log in before creating a profile", async () => {
-    const db = testEnv.unauthenticatedContext().firestore()
+    const db = testEnv.unauthenticatedContext().firestore();
     const profile = db.collection("users").doc("alice");
-    await expectFirestorePermissionDenied(profile.set({ birthday: "January 1" }))
+    await expectFirestorePermissionDenied(
+      profile.set({ birthday: "January 1" }),
+    );
   });
 
   test("should enforce the createdAt date in user profiles", async () => {
-    const db = testEnv.authenticatedContext('alice').firestore();
+    const db = testEnv.authenticatedContext("alice").firestore();
     const profile = db.collection("users").doc("alice");
-    await expectFirestorePermissionDenied(profile.set({ birthday: "January 1" }));
+    await expectFirestorePermissionDenied(
+      profile.set({ birthday: "January 1" }),
+    );
     await expectFirestorePermissionUpdateSucceeds(
       profile.set({
         birthday: "January 1",
         createdAt: serverTimestamp(),
-      })
+      }),
     );
   });
 
   test("should only let users create their own profile", async () => {
-    const db = testEnv.authenticatedContext('alice').firestore();
+    const db = testEnv.authenticatedContext("alice").firestore();
     await expectFirestorePermissionUpdateSucceeds(
       db.collection("users").doc("alice").set({
         birthday: "January 1",
         createdAt: serverTimestamp(),
-      })
+      }),
     );
     await expectFirestorePermissionDenied(
       db.collection("users").doc("bob").set({
         birthday: "January 1",
         createdAt: serverTimestamp(),
-      })
+      }),
     );
   });
 
   test("should let anyone read any profile", async () => {
-    const db = testEnv.unauthenticatedContext().firestore()
+    const db = testEnv.unauthenticatedContext().firestore();
     const profile = db.collection("users").doc("alice");
     expectPermissionGetSucceeds(profile.get());
   });
 
   test("should let anyone create a room", async () => {
-    const db = testEnv.authenticatedContext('alice').firestore();
+    const db = testEnv.authenticatedContext("alice").firestore();
     const room = db.collection("rooms").doc("firebase");
     await expectFirestorePermissionUpdateSucceeds(
       room.set({
         owner: "alice",
         topic: "All Things Firebase",
-      })
+      }),
     );
   });
 
   test("should force people to name themselves as room owner when creating a room", async () => {
-    const db = testEnv.authenticatedContext('alice').firestore();
+    const db = testEnv.authenticatedContext("alice").firestore();
     const room = db.collection("rooms").doc("firebase");
     await expectFirestorePermissionDenied(
       room.set({
         owner: "scott",
         topic: "Firebase Rocks!",
-      })
+      }),
     );
   });
 
   test("should not let one user steal a room from another user", async () => {
-    const alice = testEnv.authenticatedContext('alice').firestore();
-    const bob = testEnv.authenticatedContext('bob').firestore();
+    const alice = testEnv.authenticatedContext("alice").firestore();
+    const bob = testEnv.authenticatedContext("bob").firestore();
 
     await expectFirestorePermissionUpdateSucceeds(
       bob.collection("rooms").doc("snow").set({
         owner: "bob",
         topic: "All Things Snowboarding",
-      })
+      }),
     );
 
     await expectFirestorePermissionDenied(
       alice.collection("rooms").doc("snow").set({
         owner: "alice",
         topic: "skiing > snowboarding",
-      })
+      }),
     );
   });
 });
